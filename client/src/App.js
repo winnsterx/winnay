27f681ba9131878743eeffac4a80a3263c5dca91
Winnie Xiao <winniex@stanford.edu>
2020-10-12 23:59:30 +0200

    Pic

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import winnay from './winnay.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <p>Welcome to Winnay's World</p>
+      <img src={winnay} width="500" />
     </div>
   );
 }
