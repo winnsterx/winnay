264f1900e9de96dbf58464cad907f825999a94c3
Winnie Xiao <winniex@stanford.edu>
2020-10-14 16:37:04 +0200

    Got Dock working correcting v1

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <main>
-        <Desktop />
-      </main>
+      <Desktop />
     </div>
   );
 }
--- client/src/components/Desktop.js
+++ client/src/components/Desktop.js
@@ -1,15 +1,39 @@
 import React from "react";
+import Dock from "react-osx-dock";
+import note from "../images/dock_images/notes.png";
+import mail from "../images/dock_images/mail.png";
+import spotify from "../images/dock_images/spotify.png";
+import book from "../images/dock_images/book.png";
+import nord from "../images/dock_images/nord.png";
+import directory from "../images/dock_images/directory.png";
+import firefox from "../images/dock_images/firefox.png";
+import terminal from "../images/dock_images/terminal.png";
+
 import Exhibition from "./Exhibition";
 import Software from "./Software";
-// import Dock from './Dock'
+
+const items = [note, mail, spotify, nord, book, firefox, terminal, directory];
 
 function Desktop() {
   return (
     <div className="Desktop">
-      {/* <Top /> */}
-      <Software />
-      <Exhibition />
-      {/* <Dock /> */}
+      {/* <Software />
+      <Exhibition /> */}
+
+      <Dock
+        className="Dock"
+        backgroundClassName="DockBg"
+        width={500}
+        magnification={1}
+        magnifyDirection="up"
+        debug
+      >
+        {items.map((item, index) => (
+          <Dock.Item key={index} onClick={() => console.log(item)}>
+            <img src={item} alt="dock" />
+          </Dock.Item>
+        ))}
+      </Dock>
     </div>
   );
 }
