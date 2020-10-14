3498ccb23229721e9eaf7de8903eac485f928221
Winnie Xiao <winniex@stanford.edu>
2020-10-14 17:22:07 +0200

    Moved around Dock into DockContainer

--- client/src/components/Desktop.js
+++ client/src/components/Desktop.js
@@ -1,43 +1,16 @@
 import React from "react";
-import Dock from "react-osx-dock";
-import note from "../images/dock_images/notes.png";
-import mail from "../images/dock_images/mail.png";
-import spotify from "../images/dock_images/spotify.png";
-import book from "../images/dock_images/book.png";
-import nord from "../images/dock_images/nord.png";
-import directory from "../images/dock_images/directory.png";
-import firefox from "../images/dock_images/firefox.png";
-import terminal from "../images/dock_images/terminal.png";
 
 import Exhibition from "./Exhibition";
 import Software from "./Software";
-
-const items = [note, mail, spotify, nord, book, terminal, firefox, directory];
+import Dock from "./Dock";
+import DockContainer from "./Dock";
 
 function Desktop() {
   return (
     <div className="Desktop">
-      {/* <Software /> */}
+      <Software />
       <Exhibition />
-      <div className="dock">
-        <Dock
-          backgroundClassName="DockBg"
-          width={580}
-          magnification={0.5}
-          magnifyDirection="up"
-          // debug
-        >
-          {items.map((item, index) => (
-            <Dock.Item
-              className="DockItem"
-              key={index}
-              onClick={() => console.log(item)}
-            >
-              <img src={item} alt="dock" />
-            </Dock.Item>
-          ))}
-        </Dock>
-      </div>
+      <DockContainer />
     </div>
   );
 }
--- client/src/components/Dock.js
+++ client/src/components/Dock.js
@@ -1,6 +1,52 @@
 import React from "react";
+import Dock from "react-osx-dock";
+import finder from "../images/dock_images/finder.png";
+import note from "../images/dock_images/notes.png";
+import mail from "../images/dock_images/mail.png";
+import spotify from "../images/dock_images/spotify.png";
+import book from "../images/dock_images/book.png";
+import nord from "../images/dock_images/nord.png";
+import directory from "../images/dock_images/directory.png";
+import firefox from "../images/dock_images/firefox.png";
+import terminal from "../images/dock_images/terminal.png";
+import trash from "../images/dock_images/trash.png";
 
-function Dock() {
-  return <div className="Dock"></div>;
+// See https://lukehorvat.github.io/react-osx-dock/ for example usage
+
+const items = [
+  finder,
+  note,
+  mail,
+  spotify,
+  nord,
+  book,
+  terminal,
+  firefox,
+  directory,
+  trash,
+];
+
+function DockContainer() {
+  return (
+    <div className="dock">
+      <Dock
+        backgroundClassName="DockBg"
+        width={800}
+        magnification={0.5}
+        magnifyDirection="up"
+        // debug
+      >
+        {items.map((item, index) => (
+          <Dock.Item
+            className="DockItem"
+            key={index}
+            onClick={() => console.log(item)}
+          >
+            <img src={item} alt="dock" />
+          </Dock.Item>
+        ))}
+      </Dock>
+    </div>
+  );
 }
-export default App;
+export default DockContainer;
