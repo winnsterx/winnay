43cf269478814cbacd089e7deacc2b88c22cc11c
Winnie Xiao <winniex@stanford.edu>
2020-11-06 00:21:41 +0100

    Added info to terminal

--- client/src/components/windows/TerminalContainer.js
+++ client/src/components/windows/TerminalContainer.js
@@ -24,18 +24,48 @@ function TerminalContainer({ setTerminal }) {
           },
         }}
         commands={{
-          ls: (args, print, runCommand) => {
+          projects: (args, print, runCommand) => {
             print(
               "recent projects:\nArtxSpot      weatherFM    qinti\nwinnay.com    pintos    fibbing_reproduction\n\ncd [project_name] to learn more about specific projects"
             );
           },
+          bg: (args, print, runCommand) => {
+            print(
+              "Stanford University, Palo Alto CA\nB.S. in Computer Science (Computer Systems Track) & B.A. in Art History\nExpected to graduate in June 2022 (currently taking a gap year in Florence Italy ʕ•ᴥ•ʔ)"
+            );
+          },
+          contact: (args, print, runCommand) => {
+            print(
+              "try {\n" +
+                "    winniex at stanford period edu\n" +
+                "} catch (error) {\n" +
+                "    plus one six five zero five seven five eight six two five\n" +
+                "}"
+            );
+          },
           skills: (args, print, runCommand) => {
             print(
-              "Languages: JavaScript, Rust, C, C++, Python, TypeScript, Java, Git, Ruby on Rails, SQL\n\n" +
-                "Frameworks / APIs: React, React Native, Node, Express, MongoDB, Jekyll, Styled-Components, OpenCV, LaTex, Lua, Julia\n\n" +
-                "Tools: Docker, Docker-Compose, Kubernetes, Firebase, Heroku, GCP, AWS, MySQL, Google Analytics\n\n" +
-                "Software: SolidWorks, Fusion, Microsoft Office Suite\n\n" +
-                "Spoken languages: Mandarin, Cantonese, Japanese, & currently learning Italian"
+              "Languages: JavaScript, Rust, C, C++, Python, TypeScript, Java, SQL, Ruby on Rails\n" +
+                "Frameworks / APIs: React, React Native, Node, Express, MongoDB, Jekyll, Styled-Components, OpenCV, LaTex, Lua, Julia\n" +
+                "Tools: Docker, Docker-Compose, Kubernetes, GCP, Firebase, Heroku, Netlify, AWS, Expo, MySQL, Google Analytics\n" +
+                "Software: EAGLE, SolidWorks, Fusion, Adobe Photoshop & Illustrator, Microsoft Office Suite\n" +
+                "Spoken languages: Mandarin, English, Japanese, Cantonese, & learning Italian"
+            );
+          },
+          experiences: (args, print, runCommand) => {
+            print(
+              "Google, Chrome Platform Security         Software Engineering Intern, June to Sept 2020\n" +
+                "       * Conducted experiments of using Rust to improve memory safety in Chromium\n" +
+                "       * Worked with open-source tools to translate some critical libraries to Rust\n" +
+                "       * Created unit tests to ensure retenton of full functionality post-conversion\n" +
+                "Reach Labs                               Software Engineering Intern, June to Sept 2019\n" +
+                "       * Created tracking program with OpenCV to real-time report probe’s location\n" +
+                "       * Built ML models (Keras & scikit-learn) to classify probes and emblems\n" +
+                "       * Architected multiprocessing Python programs to halt powerful charging\n" +
+                "         station upon detection of human\n" +
+                "Stanford Microwave Integrated Circuits Lab        Research Assistant, June to Sept 2018\n" +
+                "       * Prototyped and assembled 2.45GHz RADAR PCB & phase shifter PCB\n" +
+                "       * Coded Python scripts to convert audio signals to radiowaves at set frequencies\n"
             );
           },
           cd: {
@@ -56,6 +86,10 @@ function TerminalContainer({ setTerminal }) {
                 print(
                   "You are on winnay.com, designed and built by Winnie X. Implemented with React, Styled-Components, Express, Node, and sundry other libraires / frameworks."
                 );
+              } else if (args._[0].toLowerCase().includes("weatherfm")) {
+                print(
+                  "IN PROGRESS. Fun alarm that rings sounds based on your location, time, and weather data. Built with Magenta.js, React, MongoDB, and Express. Currently in deployment stage."
+                );
               } else {
                 print("specify a project using cd [project_name]");
               }
@@ -63,12 +97,14 @@ function TerminalContainer({ setTerminal }) {
           },
         }}
         descriptions={{
-          ls: "shows all projects",
+          projects: "shows all projects",
           cd: "learn more about a specific project via 'cd [project_name]'",
           skills: "shows existing skills",
         }}
         msg={
-          "Last login: " + new Date() + ". \nUse help to see list of commands."
+          "Last login: " +
+          new Date() +
+          ". \nUse help to see list of commands.\nTry one of these commands: contact, bg, skills, experiences, projects, cd [project_name]."
         }
       />
     </div>
